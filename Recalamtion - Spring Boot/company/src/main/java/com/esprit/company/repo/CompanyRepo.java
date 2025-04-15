df1aecde51df4c7bcf136ee382368e090f3f1057
ImpacT. <100585069+Z4rga@users.noreply.github.com>
2025-04-15 17:50:15 +0100

    Sprint 2

--- Recalamtion - Spring Boot/company/src/main/java/com/esprit/company/repo/CompanyRepo.java
+++
@@ -1,9 +0,0 @@
-package com.esprit.company.repo;
-
-import com.esprit.company.entity.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepo extends JpaRepository<Company, Long> {
-}
--- Reclamation - Angular/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8087/api', // Backend URL
-    wsUrl: 'ws://localhost:8087/video-call' // WebSocket URL
-  };
-  
